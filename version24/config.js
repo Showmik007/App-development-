ec78d449cc65b75f565badb3a0f32043635f6b8f
MUSFIQUL HAMID SHOWMIK <52135967+Showmik007@users.noreply.github.com>
2026-04-06 23:06:24 +0600

    Update config.js
    Signed-off-by: MUSFIQUL HAMID SHOWMIK <52135967+Showmik007@users.noreply.github.com>

--- version24/config.js
+++ version24/config.js
@@ -15,7 +15,7 @@ window.APP_CONFIG = {
     VIDEO: 'Video Player',
     MYPOST: 'My Post'
   },
-  API_URL: https://script.google.com/macros/s/AKfycbwSH8UmHBBvRY8pK8PdWzGsP-BsQfc3hqgPkGwVFoTB0oIyFbLeqA9LWyOSd8iVJtO9/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwSH8UmHBBvRY8pK8PdWzGsP-BsQfc3hqgPkGwVFoTB0oIyFbLeqA9LWyOSd8iVJtO9/exec',
   TOKEN_KEYS: {
     USER: 'USER_TOKEN',
     ADMIN: 'ADMIN_TOKEN'
